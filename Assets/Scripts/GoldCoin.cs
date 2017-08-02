7e63cec35627a4cff5811dfb781484e4952b6600
NimbusNiles <nielspetterson@gmail.com>
2017-08-02 09:13:32 +0200

    Added spin to coins through code
    Known Issues:
     - coins are too small

--- Assets/Scripts/GoldCoin.cs
+++ Assets/Scripts/GoldCoin.cs
@@ -8,6 +8,11 @@ public class GoldCoin : MonoBehaviour {
     public int coinValue = 1;
 
     public static event Action<int> OnGoldPickup;
+   
+    private float spinTime = 1f;
+    private float spinDelta = 2f;
+    private float spinDirection = -1;
+    private Vector3 scaleVector = Vector3.one;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
@@ -15,5 +20,21 @@ public class GoldCoin : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-    
+
+    private void Update() {
+        
+        scaleVector.x = Mathf.Clamp(scaleVector.x + Time.deltaTime * spinDelta * spinDirection, 0f, 1f);
+
+        if (scaleVector.x >= 1) {
+            spinDirection = -1;
+        }else if(scaleVector.x <= 0) {
+            spinDirection = 1;
+        }
+
+        Debug.Log(scaleVector);
+
+        transform.localScale = scaleVector;
+
+    }
+
 }
