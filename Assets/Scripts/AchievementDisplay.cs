61c33e70c08885777d04d48d11e3b49a18c11928
NimbusNiles <nielspetterson@gmail.com>
2017-08-04 17:16:59 +0200

    Start to achievement system

---
+++ Assets/Scripts/AchievementDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AchievementDisplay : MonoBehaviour {
+    
+    public string achievementKey;
+
+    public Sprite lockedSprite, unlockedSprite;
+    public Slider slider;
+    public TextMeshProUGUI achievementAmount;
+
+	// Use this for initialization
+	void Start () {
+        if (achievementKey == null) {
+            Debug.LogWarning("No achievemen key found for: " + gameObject);
+        }
+        
+        if (achievementAmount != null) {
+            UpdateAmountDisplay();
+        }
+
+	}
+
+    void UpdateAmountDisplay() {
+        achievementAmount.text = PlayerPrefs.GetInt(achievementKey).ToString();
+    }
+	
+}
